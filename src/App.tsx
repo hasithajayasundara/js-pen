396cd0673b72a57e68df91305e90d8ed1ef0d4f5
hasithajayasundara <hasithajayasundara1@gmail.com>
2023-09-19 21:33:04 +0200

    add caching and dynamic package fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import * as esbuild from 'esbuild-wasm'
+  ;
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+const App = () => {
+  const [input, setInput] = useState('');
+  const esbuildInitialized = useRef(false);
+  const effectCalled = useRef(false);
+  const [code, setCode] = useState('');
 
-function App() {
-  const [count, setCount] = useState(0)
+  const onClick = async () => {
+    if (!esbuildInitialized.current) {
+      return;
+    }
+    const result = await esbuild.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        global: 'window',
+      },
+    });
+    setCode(result.outputFiles[0].text);
+  };
+
+  const startService = async () => {
+    try {
+      await esbuild.initialize({ wasmURL: '/esbuild.wasm' });
+      esbuildInitialized.current = true;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!effectCalled.current) {
+      startService();
+      effectCalled.current = true;
+    }
+  }, []);
 
   return (
-    <>
+    <div>
+      <textarea onChange={(e) => setInput(e.target.value)} />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={onClick}>Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <pre>{code}</pre>
+    </div>
   )
 }
 
---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,57 @@
+import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+  name: 'file-cache',
+});
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
+          };
+        }
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: `
+              import React, { useState } from 'react';
+              console.log(React, useState);
+            `,
+          };
+        }
+        const cached = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        if (cached) {
+          return cached;
+        }
+        const { data, request } = await axios.get(args.path);
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+        fileCache.setItem(args.path, result);
+        return result;
+      });
+    },
+  };
+};
