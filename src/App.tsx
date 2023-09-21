9bc554dfd138063013c6d5d0f3240fa3503703d6
hasithajayasundara <hasithajayasundara1@gmail.com>
2023-09-22 01:21:25 +0200

    add iframes

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,34 @@ import { unpkgPathPlugin, fetchPlugin } from './plugins';
 const App = () => {
   const [input, setInput] = useState('');
   const esbuildInitialized = useRef(false);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
   const effectCalled = useRef(false);
-  const [code, setCode] = useState('');
+
+  const html = `
+    <html>
+      <head></head>
+        <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event)=>{
+            try{
+              eval(event.data);
+            }catch(err){
+              console.error(err);
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color:red"><h4>Runtime error</h4>' + err + '</div>';
+            }
+          }, false);
+        </script>
+        </body>
+    </html>
+  `;
 
   const onClick = async () => {
-    if (!esbuildInitialized.current) {
+    if (!esbuildInitialized.current
+      || !iframeRef.current
+      || !iframeRef.current.contentWindow
+    ) {
       return;
     }
     const result = await esbuild.build({
@@ -22,12 +45,15 @@ const App = () => {
         global: 'window',
       },
     });
-    setCode(result.outputFiles[0].text);
+    iframeRef.current.srcdoc = html;
+    iframeRef.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
 
   const startService = async () => {
     try {
-      await esbuild.initialize({ wasmURL: 'https://unpkg.com/esbuild-wasm@0.19.3/esbuild.wasm' });
+      await esbuild.initialize({
+        wasmURL: 'https://unpkg.com/esbuild-wasm@0.19.3/esbuild.wasm',
+      });
       esbuildInitialized.current = true;
     } catch (err) {
       console.log(err);
@@ -47,7 +73,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        title="code preview"
+        ref={iframeRef}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   )
 }
